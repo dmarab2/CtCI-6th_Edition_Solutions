83372f2fe5339eefe73c3f9e2f2588cc3a3b4613
dmarab2 <djmarable14@yahoo.com>
2018-07-17 15:23:01 -0500

    Add files via upload

---
+++ cracking the coding interview questions/1 Arrays and Strings/3_URLify.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class URLify {
+	private StringBuilder urlify(String string){
+		StringBuilder urlArray = new StringBuilder();
+		boolean spaceAdded = false;
+		for(int i = 0; i < string.length(); i++) {
+			if(string.charAt(i) == ' ') {
+				if (spaceAdded == false) {
+					urlArray.append('%');
+					urlArray.append('2');
+					urlArray.append('0');
+				}
+				else {
+					continue;
+				}
+			}
+			else {
+				urlArray.append(string.charAt(i));
+			}
+		}
+		return urlArray;
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String urlString = new String();
+		System.out.println("Please enter the string you wish to URLify: ");
+		urlString = scan.nextLine();
+		URLify urlObj = new URLify();
+		System.out.println("Input: " + urlString);
+		System.out.println("Output: " + urlObj.urlify(urlString).toString());
+
+	}
+
+}
